7282330c501a9da7fe8a2db5f32e816d4985ebe5
Oskop <oskop17@gmail.com>
2020-05-13 03:56:49 +0700

    The Work Begin

--- GreenDoctor/app/src/main/java/isomora/com/greendoctor/Classifier.kt
+++ GreenDoctor/app/src/main/java/isomora/com/greendoctor/Classifier.kt
@@ -21,7 +21,6 @@ class Classifier(assetManager: AssetManager, modelPath: String, labelPath: Strin
     private val IMAGE_STD = 255.0f
     private val MAX_RESULTS = 3
     private val THRESHOLD = 0.4f
-    private val QUANT = false
 
     data class Recognition(
         var id: String = "",
